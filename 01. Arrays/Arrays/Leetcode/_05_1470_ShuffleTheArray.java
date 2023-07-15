baffec7da023bb60fe96dc3a9d45312c04bf20ec
unknown <harshdeep0029@gmail.com>
2023-07-15 23:49:41 +0530

    Adding leetcode arrays problems kk

---
+++ 01. Arrays/Arrays/Leetcode/_05_1470_ShuffleTheArray.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class _05_1470_ShuffleTheArray {
+    public static void main(String[] args) {
+        int[] nums = {2,5,1,3,4,7};
+        int n = 3;
+        int[] res = shuffle(nums, n);
+        System.out.println(Arrays.toString(res));        
+    }   
+    public static int[] shuffle(int[] nums, int n) {
+        int[] ans = new int[nums.length];
+
+        for (int i = 0; i < nums.length/2; i++) {
+            ans[2*i] = nums[i];
+            ans[2*i+1] = nums[i+n];
+        }
+        return ans;
+    } 
+}
---
+++ 01. Arrays/Arrays/Leetcode/_06_1431_KidsWithGreatestNoOfCandies.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class _06_1431_KidsWithGreatestNoOfCandies {
+    public static void main(String[] args) {
+        int[] candies = {4,2,1,1,2};
+        // {2,3,5,1,3} - 3(extraCandies) , {12,1,12} - 10(extraCandies)
+        int extraCandies = 1;
+        List<Boolean> result = kidsWithCandies(candies, extraCandies);
+        System.out.println(result);
+    }
+    // public static boolean[] kidsWithCandies(int[] candies, int extraCandies) {
+    //     boolean[] result = new boolean[candies.length];
+    //     int max =0;
+    //     for (int i = 0; i < candies.length; i++) {
+    //         if(candies[i] > max){
+    //             max = candies[i];
+    //         }
+    //     }
+    //     for (int i = 0; i < result.length; i++) {
+    //         if(candies[i] + extraCandies >= max){
+    //             result[i] = true;
+    //         }else{
+    //             result[i] = false;
+    //         }
+    //     }
+    //     return result;   
+    // }
+
+    public static List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        int maxCandies =0;
+
+        for (int candy : candies) {
+            maxCandies = Math.max(candy, maxCandies);
+        }
+
+        List<Boolean> result = new ArrayList<>();
+        for (int candy : candies) {
+            result.add(candy + extraCandies >= maxCandies);
+        }  
+        return result;
+    }
+}
