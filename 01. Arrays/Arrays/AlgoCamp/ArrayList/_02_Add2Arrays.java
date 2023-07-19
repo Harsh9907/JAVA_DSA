b2c2eceb4fda5956048eaae557ebcabc117e7fa8
unknown <harshdeep0029@gmail.com>
2023-07-19 11:58:13 +0530

    Adding leetcode problems

--- 01. Arrays/Arrays/AlgoCamp/ArrayList/_02_Add2Arrays.java
+++ 01. Arrays/Arrays/AlgoCamp/ArrayList/_02_Add2Arrays.java
@@ -1,21 +1,36 @@
-package ArrayList;
 import java.util.ArrayList;
 
-public class _02_Add2Arrays {
+public class Add2Arrays {
     public static void main(String[] args) {
-        int[] arr1 = {1,2,3,4,5};
-        int[] arr2 = {6,7,8,9,10};
-        ArrayList<Integer> result = add2Arrays(arr1,arr2);
-        System.out.println(result); 
+        int[] one = {9, 9};
+        int[] two = {9, 9, 9};
+        System.out.println(sumOfTwoArrays(one, two));
     }
-    public static ArrayList<Integer> add2Arrays(int[] arr1, int[] arr2) {
-        ArrayList<Integer> list = new ArrayList<>();
-        for (int i = 0; i < arr1.length; i++) {
-            list.add(arr1[i]);
+    private static ArrayList<Integer> sumOfTwoArrays(int[] one, int[] two) {
+
+        ArrayList<Integer> ans = new ArrayList<>();
+        int carry = 0;
+        int i = one.length - 1;
+        int j = two.length - 1;
+        while(i >= 0 || j >= 0) {
+            int sum = 0;
+            if(i >= 0) {
+                sum += one[i];
+            }
+            if(j >= 0) {
+                sum += two[j];
+            }
+            sum += carry;
+            int rem = sum % 10;
+            carry = sum / 10;
+            ans.add(0, rem);
+            i--;
+            j--;
         }
-        for (int i = 0; i < arr2.length; i++) {
-            list.add(arr2[i]);
+        if(carry != 0) {
+            ans.add(0, carry);
         }
-        return list;
+
+        return ans;
     }
 }
