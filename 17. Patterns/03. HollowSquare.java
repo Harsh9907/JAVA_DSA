9f5c7515c8c48757e34f76cd45e1ec89bd65a0bc
unknown <harshdeep0029@gmail.com>
2023-07-31 10:45:21 +0530

    Adding patterns problems

--- 17. Patterns/03. HollowSquare.java
+++ 17. Patterns/03. HollowSquare.java
@@ -8,8 +8,7 @@ public class GeeksForGeeks {
 		int i, j;
 		for (i = 0; i < n; i++) {
 			for (j = 0; j < n; j++) {
-				if (i == 0 || j == 0 || i == n - 1
-					|| j == n - 1) {
+				if (i == 0 || j == 0 || i == n - 1 || j == n - 1) {
 					System.out.print("*");
 				}
 				else {
