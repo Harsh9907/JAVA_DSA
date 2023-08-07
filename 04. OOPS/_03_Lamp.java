a5ae3d0cea640ff5abd0fde097e470b792af5325
unknown <harshdeep0029@gmail.com>
2023-08-07 20:39:24 +0530

    Adding oops algocamp

--- 04. OOPS/_03_Lamp.java
+++ 04. OOPS/_03_Lamp.java
@@ -1,13 +1,20 @@
 public class _03_Lamp {
     public boolean isOn;
+    private String name;
 
     public boolean isTurnOn() {
         isOn = true;
         return isOn;
     }
 
-    public boolean isTurnOff() {
-        isOn = false;
-        return isOn;
+    _03_Lamp() { // no arg constructor
+        System.out.println("Both lamp is turned on");
     }
+
+    _03_Lamp(String lang) { // parameterzed constructor
+        name = lang;
+        System.out.println(name + " is a programming language");
+    }
+    // Default constructor , if we dont create any constructor, then java
+    // automatically create the default compiler
 }
--- 04. OOPS/_04_LampClient.java
+++ 04. OOPS/_04_LampClient.java
@@ -1,14 +1,15 @@
 public class _04_LampClient {
     public static void main(String[] args) {
         _03_Lamp led = new _03_Lamp();
+        _03_Lamp halogen = new _03_Lamp("java");
         boolean result = led.isTurnOn();
         if (result) {
             System.out.println("Led Lamp is On");
         }
 
-        boolean result1 = led.isTurnOff();
-        if (result1 == false) {
-            System.out.println("Led lamp is Off");
+        boolean result1 = halogen.isTurnOn();
+        if (result1 == true) {
+            System.out.println("Halogen lamp is On.");
         }
     }
 }
