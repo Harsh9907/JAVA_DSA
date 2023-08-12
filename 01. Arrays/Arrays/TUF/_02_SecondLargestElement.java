93fb7ceeafd2eb1108da24abb3c1d188b3c9985e
unknown <harshdeep0029@gmail.com>
2023-08-12 23:19:21 +0530

    Adding TUF Arrays Problems

--- 01. Arrays/Arrays/TUF/_02_SecondLargestElement.java
+++ 01. Arrays/Arrays/TUF/_02_SecondLargestElement.java
@@ -1,3 +1,20 @@
+import java.util.Arrays;
+
 public class _02_SecondLargestElement {
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 5, 6 };
+        System.out.println("The Second Largest element present : " + secondLargest(arr));
+    }
+    // Time Complexity = O(nlogn) 
+    // Space Complexity = O(1)
+    public static int secondLargest(int[] arr) {
+        int secondLargest = Integer.MIN_VALUE;
+
+        // If array does not contain any duplicate elements
+        Arrays.sort(arr);
+        secondLargest = arr[arr.length - 2];
+        return secondLargest;
+    }
+
 
 }
