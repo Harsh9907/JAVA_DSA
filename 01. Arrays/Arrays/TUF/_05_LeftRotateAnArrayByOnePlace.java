05a894af6653e64f56557e1bc63b6f4433b94807
unknown <harshdeep0029@gmail.com>
2023-08-14 00:18:43 +0530

    Adding TUF arrays problems

--- 01. Arrays/Arrays/TUF/_05_LeftRotateAnArrayByOnePlace.java
+++ 01. Arrays/Arrays/TUF/_05_LeftRotateAnArrayByOnePlace.java
@@ -1,3 +1,17 @@
-public class _05_LeftRotateAnArrayByOnePlace {
+import java.util.*;
 
+public class _05_LeftRotateAnArrayByOnePlace {
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 5 };
+        leftRotation(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void leftRotation(int[] arr) {
+        int temp = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            arr[i - 1] = arr[i];
+        }
+        arr[arr.length - 1] = temp;
+    }
 }
