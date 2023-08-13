45944ea774122cf7b27f8a6367f32e63525ea279
unknown <harshdeep0029@gmail.com>
2023-08-14 01:15:40 +0530

    Adding TUF Arrays Problems

--- 01. Arrays/Arrays/TUF/_08_LinearSearch.java
+++ 01. Arrays/Arrays/TUF/_08_LinearSearch.java
@@ -1,3 +1,16 @@
 public class _08_LinearSearch {
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 5, 6, 14, 13, 12 };
+        System.out.println(linearSearch(arr, 14));
+    }
 
+    public static int linearSearch(int[] arr, int target) {
+        int n = arr.length - 1;
+        for (int i = 0; i <= n; i++) {
+            if (arr[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
 }
