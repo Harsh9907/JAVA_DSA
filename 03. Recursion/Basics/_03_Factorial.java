1ff78366513ee7d3fa899bfa698bf62c3bd02a0d
unknown <harshdeep0029@gmail.com>
2023-08-16 10:50:29 +0530

    Adding Recursion Problem

--- 03. Recursion/Basics/_03_Factorial.java
+++ 03. Recursion/Basics/_03_Factorial.java
@@ -1,11 +1,11 @@
 public class _03_Factorial {
     public static void main(String[] args) {
-        int num = 2;
+        int num = 0;
         System.out.println(factorial(num));
     }
 
     public static int factorial(int num) {
-        if (num == 1) {
+        if (num == 0 || num == 1) {
             return 1;
         }
         return num * factorial(num - 1);
