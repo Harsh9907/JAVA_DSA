b6512732a70226fe3127d0a810460bc9fd2dd972
unknown <harshdeep0029@gmail.com>
2023-08-18 23:01:41 +0530

    Adding TUF arrays problem

--- 01. Arrays/Arrays/TUF/_10_FindMissingNumberInAnArray.java
+++ 01. Arrays/Arrays/TUF/_10_FindMissingNumberInAnArray.java
@@ -1,7 +1,26 @@
 public class _10_FindMissingNumberInAnArray {
     public static void main(String[] args) {
         int[] arr = { 1, 2, 3, 5, 6, 7 };
+        int n = 7;
+        System.out.println(missingNum(arr, n));
         System.out.println(missingNumber(arr));
+        System.out.println(missingNumXOR(arr));
+    }
+
+    public static int missingNum(int[] arr, int n) {
+        for (int i = 1; i <= n; i++) {
+            int flag = 0;
+            for (int j = 0; j < n - 1; j++) {
+                if (arr[j] == i) {
+                    flag = 1;
+                    break;
+                }
+            }
+            if (flag == 0) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     public static int missingNumber(int[] arr) {
@@ -10,5 +29,17 @@ public class _10_FindMissingNumberInAnArray {
             sum += i;
         }
         return (((n + 1) * (n + 2)) / 2) - sum;
+    }
+
+    public static int missingNumXOR(int[] arr) {
+        int n = arr.length;
+        int xor1 = 0, xor2 = 0;
+        for (int i = 0; i < n - 1; i++) {
+            xor2 = xor2 ^ arr[i];
+            xor1 = xor1 ^ (i + 1);
+        }
+        xor1 = xor1 ^ n;
+
+        return xor1 ^ xor2;
     }
 }
