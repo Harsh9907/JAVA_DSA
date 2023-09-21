6b67205e83590e690aaf02dc25e601e1bf35338f
Harsh9907 <harshdeep0029@gmail.com>
2023-09-21 16:32:47 +0530

    changing space complexity value

--- 01. Arrays/Arrays/TUF/_04_RemoveDuplicates.java
+++ 01. Arrays/Arrays/TUF/_04_RemoveDuplicates.java
@@ -4,14 +4,14 @@ import java.util.HashSet;
 public class _04_RemoveDuplicates {
     public static void main(String[] args) {
         int[] arr = { 1, 1, 2, 2, 2, 3, 3, 4, 4 };
-        System.out.println(removeDuplicates(arr));
-        System.out.println(Arrays.toString(arr));
+        // System.out.println(removeDuplicates(arr));
+        // System.out.println(Arrays.toString(arr));
         System.out.println(removeDuplicates1(arr));
         System.out.println(Arrays.toString(arr));
     }
 
     // Time Complexity= O(nlogn + n)
-    // Space Complexity = O(1)
+    // Space Complexity = O(n) (using hashset)
     public static int removeDuplicates(int[] arr) {
         HashSet<Integer> set = new HashSet<>();
         for (int i = 0; i < arr.length; i++) {
