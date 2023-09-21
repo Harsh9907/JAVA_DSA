05506d39ff573f0b7fecab60568159bcfac7b5f3
Harsh9907 <harshdeep0029@gmail.com>
2023-09-21 19:23:35 +0530

    Adding TUF arrays Problem

--- 01. Arrays/Arrays/TUF/_15_2SumProblem.java
+++ 01. Arrays/Arrays/TUF/_15_2SumProblem.java
@@ -3,12 +3,29 @@ import java.util.Arrays;
 public class _15_2SumProblem {
     public static void main(String[] args) {
         int[] arr = { 2, 6, 5, 8, 11 };
-        int target = 8;
+        int target = 14;
         int[] ans = twoSum(arr, target);
         System.out.println(Arrays.toString(ans));
     }
 
+    // Time Complexity = O(n^2)
+    // Space Complexity = O(1) for solving the problem.
     public static int[] twoSum(int[] arr, int target) {
+        for (int i = 0; i < arr.length-1; i++) {
+            for (int j = i+1; j < arr.length; j++) {
+                if(i!=j){
+                    if(arr[i] + arr[j] == target ){
+                        return new int[]{i,j};
+                    }
+                }
+            }
+        }
+        return new int[]{-1,-1};
+    }
+
+    // Time Complexity = O(n)
+    // Space Complexity = O(1) for solving the problem.
+    public static int[] twoSum1(int[] arr, int target) {
         int left = 0, right = arr.length - 1;
         while (left < right) {
             int sum = arr[left] + arr[right];
@@ -21,5 +38,5 @@ public class _15_2SumProblem {
             }
         }
         return new int[] { -1, -1 };
-    }
+    // }
 }
