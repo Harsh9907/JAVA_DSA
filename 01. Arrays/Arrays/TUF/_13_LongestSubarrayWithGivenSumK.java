d05c920bb7466a3e4bf7bc34ad9acebb11cf9208
Harsh9907 <harshdeep0029@gmail.com>
2023-09-26 12:34:06 +0530

    Adding TUF arrays problems

--- 01. Arrays/Arrays/TUF/_13_LongestSubarrayWithGivenSumK.java
+++ 01. Arrays/Arrays/TUF/_13_LongestSubarrayWithGivenSumK.java
@@ -1,3 +1,23 @@
 public class _13_LongestSubarrayWithGivenSumK {
+    public static void main(String[] args) {
+        int[] a = { 2, 3, 5, 1, 9 };
+        int k = 10;
+        int len = getLongestSubarray(a, k);
+        System.out.println("The length of the longest subarray is: " + len);
+    }
 
+    public static int getLongestSubarray(int[] a, int k) {
+        int n = a.length;
+
+        int len = 0;
+        for (int i = 0; i < n; i++) {
+            int s = 0;
+            for (int j = i; j < n; j++) {
+                s += a[j];
+                if (s == k)
+                    len = Math.max(len, j - i + 1);
+            }
+        }
+        return len;
+    }
 }
--- 01. Arrays/Arrays/TUF/_15_2SumProblem.java
+++ 01. Arrays/Arrays/TUF/_15_2SumProblem.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class _15_2SumProblem {
     public static void main(String[] args) {
@@ -11,22 +12,39 @@ public class _15_2SumProblem {
     // Time Complexity = O(n^2)
     // Space Complexity = O(1) for solving the problem.
     public static int[] twoSum(int[] arr, int target) {
-        for (int i = 0; i < arr.length-1; i++) {
-            for (int j = i+1; j < arr.length; j++) {
-                if(i!=j){
-                    if(arr[i] + arr[j] == target ){
-                        return new int[]{i,j};
-                    }
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] + arr[j] == target) {
+                    return new int[] { i, j };
                 }
             }
         }
-        return new int[]{-1,-1};
+        return new int[] { -1, -1 };
+    }
+
+    // Time Complexity = O(n)
+    // Space Complexity = O(n) for using hashmap
+    public static int[] twoSum1(int[] arr, int target) {
+        int[] ans = { -1, -1 };
+        HashMap<Integer, Integer> mpp = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            int num = arr[i];
+            int moreNeeded = target - num;
+            if (mpp.containsKey(moreNeeded)) {
+                ans[0] = mpp.get(moreNeeded);
+                ans[1] = i;
+                return ans;
+            }
+            mpp.put(arr[i], i);
+        }
+        return ans;
     }
 
     // Time Complexity = O(n)
     // Space Complexity = O(1) for solving the problem.
-    public static int[] twoSum1(int[] arr, int target) {
-        // If the given array is unsorted, we have to sort it first,then time complexity = O(n + nlogn)   
+    public static int[] twoSum2(int[] arr, int target) {
+        // If the given array is unsorted, we have to sort it first,then time complexity
+        // = O(n + nlogn)
         int left = 0, right = arr.length - 1;
         while (left < right) {
             int sum = arr[left] + arr[right];
