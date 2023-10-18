78d0fa5f82d1b4b691269fdb2f2a3709d7f7386d
Harsh9907 <harshdeep0029@gmail.com>
2023-10-18 20:58:24 +0530

    Adding recursion problems

--- 03. Recursion/Basics/_05_Fibonacci.java
+++ 03. Recursion/Basics/_05_Fibonacci.java
@@ -1,14 +1,25 @@
 public class _05_Fibonacci {
     public static void main(String[] args) {
-        int num = 63;
-        int ans = fibonacci(num);
+        long num = 40;
+        fibonacci1(num);
+        long ans = fibonacci(num);
         System.out.println(ans);
     }
 
-    public static int fibonacci(int num) {
+    public static long fibonacci(long num) {
         if (num < 2) {
             return num;
         }
         return fibonacci(num - 1) + fibonacci(num - 2);
+    }
+
+    public static void fibonacci1(long num) {
+        int a = 0, b = 1, temp = 0;
+        for (int i = 2; i <= num; i++) {
+            temp = a + b;
+            a = b;
+            b = temp;
+        }
+        System.out.println(temp);
     }
 }
